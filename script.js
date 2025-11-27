e8586c8b4bd532aacf36660cef73c0ed5d075e8f
serhat özcakir <serhatozcakir35@gmail.com>
2025-11-27 15:01:31 +0100

    index.html, script.js, style.css updated and new img loaded

--- script.js
+++ script.js
@@ -19,7 +19,35 @@ const urls = [
     "https://pokeapi.co/api/v2/pokemon/18",
     "https://pokeapi.co/api/v2/pokemon/19",
     "https://pokeapi.co/api/v2/pokemon/20",
+    "https://pokeapi.co/api/v2/pokemon/21",
+    "https://pokeapi.co/api/v2/pokemon/22",
+    "https://pokeapi.co/api/v2/pokemon/23",
+    "https://pokeapi.co/api/v2/pokemon/24",
+    "https://pokeapi.co/api/v2/pokemon/25",
+    "https://pokeapi.co/api/v2/pokemon/26",
+    "https://pokeapi.co/api/v2/pokemon/27",
+    "https://pokeapi.co/api/v2/pokemon/28",
+    "https://pokeapi.co/api/v2/pokemon/29",
+    "https://pokeapi.co/api/v2/pokemon/30",
+    "https://pokeapi.co/api/v2/pokemon/31",
+    "https://pokeapi.co/api/v2/pokemon/32",
+    "https://pokeapi.co/api/v2/pokemon/33",
+    "https://pokeapi.co/api/v2/pokemon/34",
+    "https://pokeapi.co/api/v2/pokemon/35",
+    "https://pokeapi.co/api/v2/pokemon/36",
+    "https://pokeapi.co/api/v2/pokemon/37",
+    "https://pokeapi.co/api/v2/pokemon/38",
+    "https://pokeapi.co/api/v2/pokemon/39",
+    "https://pokeapi.co/api/v2/pokemon/40",  
 ]
+
+const loadingSpinner = document.getElementById('loading');
+let isLoading = false;
+window.addEventListener('load', () => {
+   PokemonData();
+});
+
+
 
 function getColor(type) {
     switch (type) {
@@ -31,62 +59,94 @@ function getColor(type) {
         case "electric": return "#F8D030";
         case "ground": return "#E0C068";
         case "fairy": return "#EE99AC";
-        case "psychic": return "#F85888";
         case "normal": return "#A8A878";
-        case "ice": return "#98D8D8";
-        case "dragon": return "#7038F8";
-        case "fighting": return "#C03028";
-        case "rock": return "#B8A038";
-        case "dark": return "#705848";
-        case "ghost": return "#705898";
         default: return "gray";
     }
 }
 
-
-function PokemonData() {
-    const pokemonCardData = urls.map(url => fetch(url).then(response => response.json()).then(data => {
-        console.log(data);
-        let pokemonCard = document.getElementById("pokemoncard");
+let visibleCount = 20;
+async function PokemonData() {
+    const pokemonCard = document.getElementById("pokemoncard");
+    spinnerLoad();
+    pokemonCard.innerHTML = ""; 
+    for (let index = 0; index < visibleCount; index++) {
+        const url = urls[index];
+        const response = await fetch(url);
+        const data = await response.json();
         const bgColor = getColor(data.types[0].type.name);
-        console.log(bgColor);
+        pokemonCard.style.animationDelay = `${index * 0.015}s`; 
         pokemonCard.innerHTML += `
-        <div  class="col-xs-12 col-sm-6 col-md-4 col-lg-3 mb-5 mt-5">
-            <div class="card">
-                <div class="card-body card-body-header bg-secondary text-white">
-                    <h5 class="card-title text-capitalize">#${data.id}</h5>
-                    <h5 class="card-title text-capitalize">${data.name}</h5>
-                </div>
-                <div style="background-color: ${bgColor};" onclick="openDialog(${data.id},'${bgColor}')" id="card-body-pokemon" class="card-body card-body-pokemon">
-                        ${data.types.map(item => {
-                            return `<button class="btn btn-sm text-white mx-1" style="background-color: ${bgColor};">${item.type.name}</button>
-                            `;
-                                })
-                        .join("")}
-                        <img  src="${data.sprites.other.home.front_default}" class="card-img-top" alt="${data.name}">
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2  mt-5 card-mobil fade-in">
+                <div class="card">
+                    <div class="card-body card-body-header bg-secondary text-white">
+                        <h5 class="card-title text-capitalize">#${data.id}</h5>
+                        <h5 class="card-title text-capitalize">${data.name}</h5>
+                    </div>
+                    <div style="background-color: ${bgColor};" 
+                         onclick="openDialog(${data.id},'${bgColor}')" 
+                         class="card-body card-body-pokemon">
+                        ${data.types.map(item => `
+                            <button class="btn btn-sm text-white mx-1" style="background-color: ${bgColor};">
+                                ${item.type.name}
+                            </button>`).join("")}
+                        <img src="${data.sprites.other.home.front_default}" class="card-img-top" alt="${data.name}">
+                    </div>
                 </div>
             </div>
-        </div>`;
-    }));
+        `;
+    }
+    await new Promise(resolve => setTimeout(resolve, 500))
+    diableSpinnerLoad()
+    
 }
-PokemonData();
+
+function handlesearch(){
+    const input = document.getElementById('input').value.trim();
+    console.log(input);
+    
+}
+handlesearch()
+
+function spinnerLoad(){
+    loadingSpinner.classList.remove('loading-spinner');
+    loadingSpinner.classList.add('loading-spinner_active')
+}
+
+function diableSpinnerLoad(){
+     loadingSpinner.classList.remove('loading-spinner_active');
+     loadingSpinner.classList.add('loading-spinner');
+}
+
+async function loadCard() {
+    if (isLoading) return; 
+    isLoading = true;
+    if (visibleCount === 20) {
+        visibleCount = 30;
+    } else if (visibleCount === 30) {
+        visibleCount = 40;
+    }
+    await PokemonData();
+    if (visibleCount >= urls.length){
+        document.getElementById('loadbutton').style.display = "none";
+    }
+    isLoading = false;
+}
 
 let modalShow = null;
-
 function openDialog(id,bgColor) {
-    const modalElement = document.getElementById('staticBackdrop');
-    const modalBodyID = document.getElementById('modalBodyID');
-    const modalHeder = document.getElementById('modalHeader');
-    const modalBodyInfo = document.getElementById('modal-body-info');
-    const modalFooter =  document.getElementById('modal-footer');
-    modalHeder.style.backgroundColor = bgColor;
+      const modalElement = document.getElementById('staticBackdrop');
+      const modalBodyID = document.getElementById('modalBodyID');
+      const modalHeder = document.getElementById('modalHeader');
+      const modalBodyInfo = document.getElementById('modal-body-info');
+      const modalFooter =  document.getElementById('modal-footer');
+      modalHeder.style.backgroundColor = bgColor;
     if (!modalShow) {
       modalShow = new bootstrap.Modal(modalElement, { backdrop: true, keyboard: true });
     }
     modalShow.show();
     let response = fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
     response.then(res => res.json()).then(data => {
-       // console.log(data);
+        console.log(data);
         modalHeder.innerHTML = `
         <div class="card-body modal-card-header">
             <h5 class="card-title text-capitalize">#${data.id}</h5>
@@ -97,12 +157,14 @@ function openDialog(id,bgColor) {
                     ${data.types.map(item => {
                     const bgColor = getColor(data.types[0].type.name);
                     return `<button class="btn btn-sm text-white mx-1 mb-2" style="background-color: ${bgColor};">${item.type.name}</button>
-                    `;})}
+                    `;
+                     }).join("")}
             </div>
-                <img src="${data.sprites.other?.showdown?.front_shiny 
+            <img src="${data.sprites.other?.showdown?.front_shiny 
                  || data.sprites.other?.home?.front_default 
                  || data.sprites.front_default}" class="card-img-top modal-image" alt="${data.name}">
         </div>
+        
         `;
         modalBodyInfo.innerHTML = `
                 <a style="color: ${bgColor};" onclick="modalAbout(${data.id})">About</a><a style="color: ${bgColor};" onclick="modalBaseStats(${data.id})">Base Stats</a><a style="color: ${bgColor}" onclick="modalShiniy(${data.id})">Shiny</a>
@@ -112,12 +174,11 @@ function openDialog(id,bgColor) {
             <button onclick="nextPokemonCard(${data.id})" style="color: ${bgColor};" type="button" class="btn"><i class="bi bi-arrow-right-circle icon"></i></button>
         `;
     });
- //  modalAbout(id)    
+   modalAbout(id)    
 }
 
 function modalAbout(id){
-      let response = fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-     console.log(`${id} tiklandi`);
+    let response = fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
     response.then(res => res.json()).then(data => {
       modalBodyID.innerHTML = `
         <div class="card-title mb-1">
@@ -136,7 +197,8 @@ function modalAbout(id){
             <div class="card-title-key">Abilities</div>
             <div class="card-title-value">: ${data.abilities.map(item=>item.ability.name[0].toUpperCase() + item.ability.name.slice(1))}</div>
         </div>      
-        `;})
+        `;
+})
 }
 
 function modalBaseStats(id){
@@ -145,17 +207,17 @@ function modalBaseStats(id){
     response.then(res => res.json()).then(data => {
     modalBodyID.innerHTML = `
          ${data.stats.map(item => `
-        <div class="card-title mb-1">
+    <div class="card-title mb-1">
             <div class="card-title-key">${item.stat.name[0].toUpperCase() + item.stat.name.slice(1)}</div>
             <div class="card-title-value">: ${item.base_stat}</div>
             <div class="progress" role="progressbar" aria-label="Success example"  aria-valuenow="" aria-valuemin="" aria-valuemax="100">
-            <div class="progress-bar bg-${item.base_stat >= 60 ? 'success' : 'danger'}" style="width:${item.base_stat}%"></div>
+            <div class="progress-bar bg-${item.base_stat > 60 ? 'success' : 'danger'}" style="width:${item.base_stat}%"></div>
             </div>
-        </div>
-    `)} `;
+    </div>
+    `).join('')}
+        `;
     })
 }
-
 function modalShiniy(id){
      let response = fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
      console.log(`${id} tiklandi`);
@@ -169,12 +231,12 @@ function modalShiniy(id){
 
 function nextPokemonCard(id){
     console.log(`${id} tiklandi`);
-    if(id < 20){
-        const nextIdNummer = id + 1;
-        let response = fetch(`https://pokeapi.co/api/v2/pokemon/${nextIdNummer}`);
+    if(id < 40){
+        const nexIdNumber = id + 1;
+        let response = fetch(`https://pokeapi.co/api/v2/pokemon/${nexIdNumber}`);
         response.then(response=> response.json()).then(data=>{
             const nextColor = getColor(data.types[0].type.name);
-            openDialog(nextIdNummer,nextColor);
+            openDialog(nexIdNumber,nextColor);
         })
     }
 }
